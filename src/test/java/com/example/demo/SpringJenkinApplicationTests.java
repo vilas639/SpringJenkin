0aab2344771ab8e3c01aeca8167f8254e0027015
vilas639 <vilasjdhv639@gmail.com>
2024-11-20 15:47:49 +0530

    testignore

--- src/test/java/com/example/demo/SpringJenkinApplicationTests.java
+++ src/test/java/com/example/demo/SpringJenkinApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class SpringJenkinApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
