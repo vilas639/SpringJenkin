c2db9179cf2e47a4740708df98aae1b9da539561
vilas639 <vilasjdhv639@gmail.com>
2024-11-20 11:21:24 +0530

    new spring jenkins

---
+++ src/main/java/com/example/demo/SpringJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinApplication.class, args);
+	}
+
+}
